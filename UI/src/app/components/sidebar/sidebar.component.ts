e82eed59607be8999d72276bd990a36bc6c876c9
rangesh <rsripathi781@gmail.com>
2020-02-28 19:48:32 +0530

    UI Changes

--- UI/src/app/components/sidebar/sidebar.component.ts
+++ UI/src/app/components/sidebar/sidebar.component.ts
@@ -9,8 +9,8 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'Trigger API',  icon:'person', class: '' },
-    { path: '/table-list', title: 'CDR List',  icon:'content_paste', class: '' }
+    { path: '/user-profile', title: 'CDR Batch Job',  icon:'person', class: '' },
+    { path: '/table-list', title: 'CDR Dispute',  icon:'content_paste', class: '' }
     // { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     // { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
     // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
